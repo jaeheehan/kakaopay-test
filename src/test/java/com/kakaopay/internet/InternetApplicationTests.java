7d4437fb084ae8bf2ef1d03f69aed5b93e4a987b
jaehee <jaehee23@naver.com>
2019-09-18 06:01:03 +0900

    first

---
+++ src/test/java/com/kakaopay/internet/InternetApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kakaopay.internet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InternetApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
